7ea0ea27913b9597dbef7aeedb969dcfe6392708
HP <HP@DESKTOP-NA1S1EJ>
2021-05-14 13:21:26 +0530

    First commit

---
+++ src/git1/test1.java
@@ -0,0 +1,17 @@
+package git1;
+
+public class test1 {
+	public static void main(String[] args) {
+		String hungry ="yes";
+		
+		if(hungry == "yes")
+		{
+			System.out.println("eat something....");
+		}
+			else {
+				System.out.println("Do work");
+			}
+		}
+	}
+
+
